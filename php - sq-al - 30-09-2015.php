8657295a202449b672798007a9798dc023ee3303
Brent <brent@qordoba.com>
2015-10-07 17:35:57 +0100

    PHp is love

--- php - sq-al - 30-09-2015.php
+++ php - sq-al - 30-09-2015.php
@@ -1,6 +1,20 @@
-<p data-l10n-context="context" data-l10n-hint="hint"></p>
+<p data-l10n-context="context" data-l10n-hint="hint">Content 1</p>
 
 <h1>string</h1>
 
 <p>string</p>
-<p lang=""></p>
+<p lang="">Content 1</p>
+<div>non-translatable string</div>
+<div lang="en">Content 1</div>
+
+<?php
+    echo "Content 2";
+    echo _("string");
+    echo __('string');
+    echo ___('string');
+?>
+
+<script type="text/javascript">
+    alert("Content 3");
+    alert(___('Content 4'));
+</script>
